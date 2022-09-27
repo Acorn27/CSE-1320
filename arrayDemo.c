090acf17c9b286a6082780a9ec2f347d5b8986b7
Acorn27 <maitran.mt02@gmail.com>
2022-09-27 09:25:29 -0500

    invisible change

---
+++ arrayDemo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int* createArray(void)
+{
+    
+}
+
+int main(void)
+{
+    
+    char MyClassName[10] = {};
+
+    printf("What is your class name: ");
+    scanf("%s", MyClassName);
+
+    printf("Your class name is: %s", MyClassName);
+    
+    return 0;
+
+}
