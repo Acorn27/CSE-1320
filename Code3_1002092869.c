0af6fcaf454660866847cfada8e0f1145165354f
Acorn27 <maitran.mt02@gmail.com>
2022-09-28 22:49:21 -0500

    Nearly done

--- Code3_1002092869.c
+++ Code3_1002092869.c
@@ -1,16 +1,309 @@
-/******************************************************************************
+/*
+Author: Mai Tran
+UTA ID: 1002092849
+Course: CSE 1320
+Last modified: 09/28/2022
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
 
-*******************************************************************************/
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #define ROW 5
-#define COLLUMN 5
+#define COLUMN 5
+
+
+// supplement function, use 1D array to keep track of used random number
+// take array, size, and test_number as parameter
+int is_unique(int[], int, int);
+
+// fill card with unique random number in proper range
+void fill_bingo_card(int[ROW][COLUMN]);
+
+// print bingo card to the screen
+void print_bingo_card(int[ROW][COLUMN]);
+
+// pick a random number that has not been chosed
+int pick_random_number(int[75]);
+
+// complement function for pick_random_number function
+// return 'B', 'I',... base on number passed to function
+char get_precedence(int);
+
+// check if number existed in player's bingo card
+int is_from_bingo(int[ROW][COLUMN], int);
+
+// check for complete row
+int is_completed_row(int[ROW][COLUMN]);
+
+int is_completed_row(int bingo[ROW][COLUMN])
+{   
+    // set is_completed to false
+    int is_completed = 0;
+
+    // condition: if row < max_row and one we haven't had any complete row yet
+    for (int row = 0; row < ROW && !(is_completed); row++)
+    {   
+        // assume at the beginning of each row that it is a completed on
+        is_completed = 1;
+        for (int col = 0; col < COLUMN; col++)
+        {   
+            // mark incomplete if one elemnt is not complete
+            if (bingo[row][col] != 0)
+            {   
+                is_completed = 0;
+            }
+        }
+    }
+    return (is_completed);
+}
+
+//check for complete collumn
+int is_complete_column(int bingo[ROW][COLUMN]);
+int is_complete_column(int bingo[ROW][COLUMN])
+{
+    // set is_completed to false
+    int is_completed = 0;
+
+    // condition: if col < max_col and one we haven't had any complete collumn yet
+    for (int col = 0; col < COLUMN && !(is_completed); col++)
+    {   
+        // assume at the beginning of each row that it is a completed on
+        is_completed = 1;
+        for (int row = 0; row < ROW; row++)
+        {
+            // mark incomplete if one elemnt is not complete
+            if (bingo[row][col] != 0)
+            {
+                is_completed = 0;
+            }
+        }
+    }
+    return (is_completed);
+}
+int check_diagnose(int[ROW][COLUMN]);
+
+
+int check_win(int bingo[ROW][COLUMN]) {
+    
+    int row_win = 0;
+    int col_win = 0;
+    int diagno_win = 1;
+    int is_win = 0;
+    
+    for (int row = 0; row < ROW && !(row_win); row++)
+    {   
+        row_win = 1;
+        for (int col = 0; col < COLUMN; col++)
+        {
+            if (bingo[row][col] != 0)
+            {
+                row_win = 0;
+            }
+        }
+    }
+    
+    for (int col = 0; col < COLUMN && !(col_win); col++)
+    {   
+        col_win = 1;
+        for (int row = 0; row < ROW; row++)
+        {
+            if (bingo[row][col] != 0)
+            {
+                col_win = 0;
+            }
+        }
+    }
+    
+    for (int i = 0; i < ROW; i++)
+    {   
+        if (bingo[i][i] != 0)
+        {
+            diagno_win = 0;
+        }
+    }
+    
+    if (diagno_win == 0)
+    {
+        
+        diagno_win = 1;
+        for (int i = 0; i < ROW; i++)
+        {      
+            if (bingo[i][ROW -i-1] != 0)
+            {
+                diagno_win = 0;
+            }
+        }
+    }
+
+
+    if (row_win && col_win && diagno_win) 
+    {
+        printf("\nYou filled out a row and a collumn and a diagno -- BINGO!!!\n");
+        is_win = 1;
+        
+    } 
+    else if (col_win && diagno_win) 
+    {
+        printf("\nYou filled out a collumn and a diagno -- BINGO!!!\n");
+        is_win = 1;
+    } 
+    else if (row_win && diagno_win) 
+    {
+        printf("\nYou filled out a row and a diagno -- BINGO!!!\n");
+        is_win = 1;
+    } 
+    else if (row_win && col_win) 
+    {
+        printf("\nYou filled out a row and a collumn -- BINGO!!!\n");
+        is_win = 1;
+    }
+    else if (row_win)
+    {
+        printf("\nYou filled out a row -- BINGO!!!\n");
+        is_win = 1;
+    }
+    else if (col_win)
+    {
+        printf("\nYou filled out a collumn -- BINGO!!!\n");
+        is_win = 1;
+    }
+    else if (diagno_win)
+    {
+        printf("\nYou filled out a diagno -- BINGO!!!\n");
+        is_win = 1;
+    } 
+
+    return (is_win);
+    
+}
+
+
+
+int main()
+{
+    srand(time(0));
+    
+    // empty bingo card
+    int bingo_array[ROW][COLUMN] = {};
+    
+    // fill bingo card function
+    fill_bingo_card(bingo_array);
+    
+    // screen bingo card to screen
+    print_bingo_card(bingo_array);
+
+    int is_win = 0;
+    int drawn_remains = 75;
+
+    // keep track of random number that has not been picked
+    int chosed_number[75] = {};
+
+    // current random number
+    int current_pick;
+
+    // user choice
+    char choice;
+
+    // game loop
+    while (!is_win && drawn_remains > 0)
+    {
+        // pick unique random number
+        current_pick = pick_random_number(chosed_number);
+
+        // update dupplicate array
+        chosed_number[75 - drawn_remains] = current_pick;
+
+        // decrease draw remaining by one
+        drawn_remains--;
+
+        //
+        printf("Do you have it ? (Y/N) ");
+        scanf("%c", &choice);
+        //printf("Choice is %c\n", choice);
+        getchar();
+        if (choice == 'Y')
+        {
+            if (is_from_bingo(bingo_array, current_pick))
+            {
+                print_bingo_card(bingo_array);
+                is_win = check_win(bingo_array);
+
+            }
+            else 
+            {
+                printf("\nThat value is not on your BINGO card - are you trying to cheat??\n");
+            }
+        } 
+        else 
+        {
+            print_bingo_card(bingo_array);
+        }
+    }
+
+    return 0;
+}
+
+void fill_bingo_card(int bingo_array[ROW][COLUMN])
+{   
+    // keep track of unique random number for each collumn, initialize to -1 
+    int dup_array[COLUMN] = {-1, -1, -1, -1, -1};
+
+    // most recent random numebr
+    int current_number = -1;
+    
+    // loop through empty array
+    for (int col = 0; col < COLUMN; col++)
+    {
+        for (int row = 0; row < ROW; row++)
+        {
+            
+            // get random number in a rangle related to collumn
+            current_number = (rand() % 15) + 15*col + 1;
+            
+            // re-create random number if the number has been previously used
+            while (!(is_unique(dup_array, COLUMN, current_number)))
+            {
+                current_number = (rand() % 15) + 15*col + 1;
+            }
+            
+            // add the new digit to track array
+            dup_array[row] = current_number;
+
+            // all previous condition meet, set this random number to array
+            bingo_array[row][col] = current_number;
+        }
+    }
+
+    // set center of board to 0
+    bingo_array[2][2] = 0;
+}
+
+void print_bingo_card(int bingo_array[ROW][COLUMN])
+{
+
+    // print the title
+    printf("    B       I       N       G       O    \n");
+    printf("-----------------------------------------\n");
+
+    // print bingo card
+    for (int row = 0; row < ROW; row++)
+    {
+        for (int col = 0; col < COLUMN; col++)
+        {
+            if (bingo_array[row][col] == 0) {
+                printf("%c%4c%3c",'|', 'X', ' ');
+            }
+            else 
+            {
+                printf("%c%4d%3c",'|', bingo_array[row][col], ' ');
+            }
+        }
+        printf("|\n");
+        printf("-----------------------------------------\n");
+    }
+}
 
 int is_unique(int dup_array[], int size, int test_number)
 {
@@ -27,59 +320,18 @@ int is_unique(int dup_array[], int size, int test_number)
     return is_unique;
 }
 
-
-void fill_bingo_card(int bingo_array[ROW][COLLUMN])
+int pick_random_number(int chosed_number[75])
 {
-    int dup_array[COLLUMN] = {-1, -1, -1, -1, -1};
-    int current_element = -1;
-    int size;
-    
-    for (int col = 0; col < COLLUMN; col++)
-    {
-        for (int row = 0; row < ROW; row++)
+
+    int ran_number = (rand() % 75 + 1);
+
+    // if number alreay been pick, pick another number
+    while (!(is_unique(chosed_number, 75, ran_number)))
         {
-            current_element = (rand() % 15) + 15*col + 1;
-            size = sizeof(dup_array) / sizeof(int);
-            
-            while (!(is_unique(dup_array, size, current_element)))
-            {
-                current_element = (rand() % 15) + 15*col + 1;
-            }
-            
-            dup_array[row] = current_element;
-            bingo_array[row][col] = current_element;
+            ran_number = rand() % 75 + 1;
         }
-    }
-}
-
-void print_bingo_card(int bingo_array[ROW][COLLUMN])
-{
-
-    // print the title
-    printf("    B       I       N       G       O    \n");
-    printf("-----------------------------------------\n");
-
-    // print bingo card
-    for (int row = 0; row < ROW; row++)
-    {
-        for (int col = 0; col < COLLUMN; col++)
-        {
-            if (bingo_array[row][col] == 0) {
-                printf("%c%4c%3c",'|', 'X', ' ');
-            }
-            else 
-            {
-                printf("%c%4d%3c",'|', bingo_array[row][col], ' ');
-            }
-        }
-        printf("|\n");
-        printf("-----------------------------------------\n");
-    }
-}
-
-int pick_random_number()
-{
-    return (rand() % 75 + 1);
+    printf("\nThe next number is %c%d\n\n", get_precedence(ran_number), ran_number);
+    return (ran_number);
 }
 
 char get_precedence(int number)
@@ -107,72 +359,23 @@ char get_precedence(int number)
     {
         precedence = 'O';
     }
+    
     return precedence;
-
 }
 
-int is_from_bingo(int bingo[ROW][COLLUMN], int test_number) 
+int is_from_bingo(int bingo[ROW][COLUMN], int test_number) 
 {
     int from_bingo = 0;
     for (int row = 0; row < ROW; row++)
     {
-        for (int col = 0; col < COLLUMN; col++)
+        for (int col = 0; col < COLUMN; col++)
         {
             if (bingo[row][col] == test_number)
-            {
+            {   
+                bingo[row][col] = 0;
                 from_bingo = 1;
             }
         }
     }
     return (from_bingo);
-}
-
-
-
-int main()
-{
-    srand(time(0));
-    
-    // empty bingo card
-    int bingo_array[5][5] = {};
-    
-    // fill bingo card
-    fill_bingo_card(bingo_array);
-    
-    // screen bingo card to screen
-    print_bingo_card(bingo_array);
-
-    int is_win = 0;
-    int drawn_remains = 75;
-    int chosed_number[75] = {};
-    int current_pick;
-    char choice;
-
-    while (!is_win && drawn_remains > 0)
-    {
-        current_pick = pick_random_number();
-
-        while (!(is_unique(chosed_number, 75 - drawn_remains, current_pick)))
-        {
-            current_pick = pick_random_number();
-        }
-
-        chosed_number[75 - drawn_remains] = current_pick;
-        drawn_remains--;
-        //printf("Pick number is: %d\n", current_pick);
-        //printf("%d. the next number is %c%d\n",75 - drawn_remains, get_precedence(current_pick), current_pick);
-        printf("\nThe next number is %c%d\n\n", get_precedence(current_pick), current_pick);
-        printf("Do you have it ? (Y/N)");
-        getchar();
-        scanf("%c", &choice);
-        if (choice == 'Y')
-        {
-            if (is_from_bingo(bingo_array, 25, current_pick))
-            {
-
-            }
-        }
-    }
-
-    return 0;
 }
