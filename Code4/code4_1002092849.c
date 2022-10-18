6c7c588f1fd6a668859e4e3fb8a3929f824278d9
Acorn27 <maitran.mt02@gmail.com>
2022-10-18 15:23:59 -0500

    bug inside loop

--- Code4/code4_1002092849.c
+++ Code4/code4_1002092849.c
@@ -9,6 +9,11 @@ Last modified: 10/18/2020
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+int valid_range(int x, int y, int length, int max, char type);
+
+void commandParse(char userInput[], char letter_command[], int* x_coordinate, int* y_coordinate,int* length, char mark[]);
 
 int main()
 {   
@@ -32,37 +37,113 @@ int main()
 
     // prompt for user command
     char userInput[20];
-    printf("Enter draw command (enter Q to quit) ");
+    printf("\nEnter draw command (enter Q to quit) ");
     fgets(userInput, 19, stdin);
 
+    // delimiter 
     char delim[] = "(),";
+    // draw command string
     char letter_command[2]= {};
     int x_coordinate,
         y_coordinate,
         length;
+    //  mark
     char mark[2] = {};
+
+    //commandParse(userInput, letter_command, &x_coordinate, &y_coordinate, &length, mark);
+
+    char* token;
+    token = strtok(userInput, delim);
+    strcpy(letter_command, token);
+    letter_command[0] = toupper(letter_command[0]);
+
+
+    while (letter_command[0] != 'Q')
+    {
+        token = strtok(NULL, delim);
+        x_coordinate = atoi(token);
+
+        token = strtok(NULL, delim);
+        y_coordinate = atoi(token);
+
+        token = strtok(NULL, delim);
+        length = atoi(token);
+
+        token = strtok(NULL, delim);
+        strcpy(mark, token);
+
+        if (valid_range(x_coordinate, y_coordinate, length, map_size, letter_command[0]))
+        {
+            DrawLine(draw_board, x_coordinate, y_coordinate, letter_command[0], length, mark[0]);
+            PrintMap(draw_board, map_size);
+            printf("\nEnter draw command (enter Q to quit) ");
+            fgets(userInput, 19, stdin);
+            token = strtok(userInput, delim);
+            strcpy(letter_command, token);
+            letter_command[0] = toupper(letter_command[0]);
+
+        } else
+        {
+            printf("That draw command is out of range\n");
+        }
+
+    }
+
+}
+
+
+int valid_range(int x, int y, int length, int max, char type)
+{
+
+    int general_test = 1,
+        case_H_test = 1,
+        case_V_test = 1;
+
+    // general test
+    if (x < 0 || x > max || y < 0 || y > max)
+    {
+        general_test = 0;
+    }
+
+    // case_test
+    if (general_test != 0)
+    {
+        if (type == 'H')
+        {
+            if (y + length > max)
+            {
+                case_H_test = 0;
+            }
+        }
+        else if (type == 'V')
+        {
+            if (x + length > max)
+            {
+                case_V_test = 0;
+            }
+        }
+
+    }
+    return (general_test && case_H_test && case_V_test);
+}
+
+void commandParse(char userInput[], char letter_command[], int* x_coordinate, int* y_coordinate,int* length, char mark[])
+{
+
+    char delim[] = "(),";
 
     char* token = strtok(userInput, delim);
     strcpy(letter_command, token);
 
     token = strtok(NULL, delim);
-    x_coordinate = atoi(token);
+    *x_coordinate = atoi(token);
 
     token = strtok(NULL, delim);
-    y_coordinate = atoi(token);
+    *y_coordinate = atoi(token);
 
     token = strtok(NULL, delim);
-    length = atoi(token);
+    *length = atoi(token);
 
     token = strtok(NULL, delim);
     strcpy(mark, token);
-
-    if (strcmp(mark, "\n") == 0)
-    {
-        strcpy(mark,"X");
-    }
-
-
-
-
 }
