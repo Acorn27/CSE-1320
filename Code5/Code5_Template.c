45a5587d791c44ab43d025c597ae1b4eaaf2cf53
Acorn27 <maitran.mt02@gmail.com>
2022-10-28 21:31:39 -0500

    Allll

--- Code5/Code5_Template.c
+++ Code5/Code5_Template.c
@@ -1,30 +1,53 @@
-/* Coding Assignment 5 */
+
+/* 
+Author: Mai Tran
+UTA ID: 1002092849
+Coding Assignment 5
+Last Modified: 10/28/2022 
+*/
+# include <stdio.h>
+# include "GameLib.h"
 
 /* Add necessary includes */
 
 int main(void)
 {
+
+	// 3 NULL arrays
 	char Phrase[MAX_INPUT] = {};
 	char DashedPhrase[MAX_INPUT] = {};
 	char UpperPhrase[MAX_INPUT] = {};
+
+	// keep track of number of strikes
 	int Strikes = 0;
 	int i;
 	
 	/* Call StartGame and pass in Phrase */
+	StartGame(Phrase);
+
 	/* Call DashIt and pass in Phrase and DashedPhrase */
+	DashIt(Phrase, DashedPhrase);
+
 	/* uppercase Phrase */
+	for (i = 0; i < strlen(Phrase); i++)
+	{
+	    UpperPhrase[i] = toupper(Phrase[i]);
+	}
 	
 	printf("\nHere's the phrase you need to guess\n");
 
 	do
 	{
 		/* if !(call GuessALetter and pass Phrase, DashedPhrase and UpperPhrase) */
+		if (!(GuessALetter(Phrase, DashedPhrase, UpperPhrase)))
 		{
 			Strikes++;
 			printf("\nStrike %d", Strikes);
 		}
 	}
 	/* while (call strchr() with DashedPhrase with -) AND strike counter is less than strikes define */
+	while (strchr(DashedPhrase,'_') && Strikes < STRIKES);
+
 
 	if (Strikes < STRIKES)
 	{
