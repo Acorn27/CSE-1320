0e85a8141169e2f6b8dc926a9ab8b69d23cde21a
Acorn27 <maitran.mt02@gmail.com>
2022-11-19 13:47:36 -0600

    test latter

--- Code6/Code6_1002092849.c
+++ Code6/Code6_1002092849.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "DrawTool.h"
 #include "FileLib.h"
 #include "ListLib.h"
@@ -6,7 +7,14 @@
 int main(int argc, char *argv[])
 {   
 
+    // Linked list head
     NODE *LinkListHead;
+    char map[MAXMAPSIZE][MAXMAPSIZE] = {};
+    char Buffer[5] = {};
+    char DC[100] = {};
+    NODE *TempPtr;
+    char draw_command[5]= {};
+    int x_coordinate, y_coordinate, length;
 
     // call OpenFIle() in main()
     FILE *MyFile;
@@ -14,6 +22,58 @@ int main(int argc, char *argv[])
 
     // call ReadFileIntoLinkedList()
     ReadFileIntoLinkedList(MyFile, &LinkListHead);
+
+    // initialize map
+    InitializeMap(map);
+
+    printf("Please enter 1-3 letters ");
+    scanf("%s", Buffer);
+
+    //for loop over strlen of numbers of letters entered
+    for (int i = 0; i < strlen(Buffer)-1; i++)
+    {
+        //uppercase the current letter
+        Buffer[i] = toupper(Buffer[i]);
+
+        // Find the first node containing your first letter and
+        // get back a bookmark to the next node
+        TempPtr = FindLetter(LinkListHead, Buffer[i], DC);
+        // Either check the strlen() of draw command (0 means it was set to NULL)
+        // or check if element [0] of draw command is '\0'
+        // You want to continue to loop while your draw command has something in it
+        while (DC[0] != NULL)
+        {
+            //tokenize and draw the command
+            // parse command letter
+            char* token;
+            token = strtok(DC, "(),");
+            strcpy(draw_command, token);
+            draw_command[0] = toupper(draw_command[0]);
+            token = strtok(NULL, "(),");
+            x_coordinate = atoi(token);
+            token = strtok(NULL, "(),");
+            y_coordinate = atoi(token);
+            token = strtok(NULL, "(),");
+            length = atoi(token);
+
+            if (draw_command[0] == 'P')
+            {
+                map[x_coordinate][y_coordinate] = Buffer[i];
+            }
+            else if (draw_command[0] == 'H' || draw_command[0] == 'V')
+            {
+                DrawLine(map, x_coordinate, y_coordinate, draw_command[0], length, Buffer[i]);
+            }
+            else
+            {
+                printf("\nThat draw command is unknown\n");
+            }
+
+            // Look for the next node containing the letter
+            // draw command will be NULL if not found
+            TempPtr = FindLetter(LinkListHead, Buffer[i], DC);
+        }
+    }
 
 
 
