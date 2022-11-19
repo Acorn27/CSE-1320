375a43a5bb5859c3329db2aa0ddc728da30ec4c9
Acorn27 <maitran.mt02@gmail.com>
2022-11-19 10:33:01 -0600

    save1

--- Code6/FileLib.c
+++ Code6/FileLib.c
@@ -4,28 +4,49 @@
 #include <string.h>
 #include "ListLib.h"
 
-/* return type */ OpenFile(/* parameters */)
+FILE *OpenFile(int argc, char *argv[])
 {	
 	/* declare various variables needed */
+	char FileName[20] = {};
+	FILE* MyFile;
 
 	/* if argc is 2, then use argv[1] as the input file name, else print the message seen in sample output */
+	if (argc == 2)
+	{
+		strcpy(FileName, argv[1]);
+	}
+	else 
+	{
+		printf("Must run with an input file name.\n");
+		printf("Enter a file name at the prompt ");
+	}
 
 	do
 	{
 		/* open file with "r" mode */
+		MyFile = fopen(FileName, "r");
+
 		/* if file did not open */
+		if (MyFile == NULL)
 		{
 			/* print message seen in sample output */
+			printf("Could not open a file named %s", FileName);
+
 			/* read in new filename */
+			printf("Enter a file name at the prompt ");
+			scanf("%s", FileName);
+
 			/* open the file "r" mode */
+			MyFile = fopen(FileName, "r");
 		}
 	}
-	while (/* file is not open */)
+	while (MyFile == NULL);
 
 	/* return the file handle */
+	return MyFile;
 }
 
-/* return type */ ReadFileIntoLinkedList(/* parameters */)
+void ReadFileIntoLinkedList(FILE *DCFile, NODE **LinkedListHead)
 {
 	/* declare various variables needed */
 	
