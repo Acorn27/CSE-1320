22c5ef78aa1638244697442a7ae615c384793cc5
Acorn27 <maitran.mt02@gmail.com>
2022-11-18 18:39:36 -0600

    CA6

---
+++ Code6/FileLib.c
@@ -0,0 +1,40 @@
+// FileLib Template
+
+#include <stdio.h>
+#include <string.h>
+#include "ListLib.h"
+
+/* return type */ OpenFile(/* parameters */)
+{	
+	/* declare various variables needed */
+
+	/* if argc is 2, then use argv[1] as the input file name, else print the message seen in sample output */
+
+	do
+	{
+		/* open file with "r" mode */
+		/* if file did not open */
+		{
+			/* print message seen in sample output */
+			/* read in new filename */
+			/* open the file "r" mode */
+		}
+	}
+	while (/* file is not open */)
+
+	/* return the file handle */
+}
+
+/* return type */ ReadFileIntoLinkedList(/* parameters */)
+{
+	/* declare various variables needed */
+	
+	/* while fgets() reads the file */
+	{
+		/* if line from file ends with \n, then replace \n with \0 */
+		
+		/* tokenize to get the Letter and the DrawCommand */
+	
+		/* Call AddDrawCommandToList with correct parameters */
+	}
+}
---
+++ Code6/ListLib.c
@@ -0,0 +1,32 @@
+/* ListLib-Template */
+
+#include "ListLib.h"
+/* add return type */ AddDrawCommandToList(/* Add parameters */)
+{
+    NODE *TempPtr, *NewNode;
+	/* declare pointers of type NODE named TempPtr and NewNode */
+    
+	/* malloc a new node and assign the Letter and the DrawCommand (after mallocing memory for it) */
+
+	/* If the LinkedListHead is NULL, then give it the address of the new node */
+	/* Else traverse to the end of the linked list and add the new node */
+}
+
+/* add return type */ FindLetter(/* Add parameters */)
+{
+	/* while traversing the linked list AND the Letter in the node is not the Letter passed in */
+	{
+		/* move TempPtr */
+	}
+
+	if (/* TempPtr is not NULL */)
+	{
+		/* copy the DrawCommand from the node into the passed in parameter */
+		/* return the next pointer stored in TempPtr */
+	}
+	else
+	{
+		memset(DC, '\0', sizeof(*DC));  //Set DC to NULLs to signal that we found all of the nodes for this letter
+		/* return TempPtr */
+	}
+}
